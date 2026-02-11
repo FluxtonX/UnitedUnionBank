b0f96f7500e67fa5d753da4f630a11fdddb5b77a
Muhammad-Ullah <muhammad.mail1625@gmail.com>
2026-02-11 19:41:14 +0500

    add camera, photo library, and microphone permissions and update onboarding images

--- lib/config/app_images.dart
+++ lib/config/app_images.dart
@@ -1,9 +1,9 @@
 class AppImages {
   static const String imagesDirectory = 'assets/images';
   static const String appLogo = "$imagesDirectory/logo.jpg";
-  static const String onboardingImage1 = "$imagesDirectory/image1.png";
-  static const String onboardingImage2 = "$imagesDirectory/image2.png";
-  static const String onboardingImage3 = "$imagesDirectory/image1.png";
+  static const String onboardingImage1 = "$imagesDirectory/logo.jpg";
+  static const String onboardingImage2 = "$imagesDirectory/logo.jpg";
+  static const String onboardingImage3 = "$imagesDirectory/logo.jpg";
 
 }
 
